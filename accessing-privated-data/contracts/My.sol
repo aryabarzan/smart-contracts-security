0dbcf4d5604097028a9a55d3e11caf9c4ae0673d
Nader Aryabarzan <aryabarzan@gmail.com>
2023-09-28 23:37:40 +0200

    Insert comment in the contract

--- accessing-privated-data/contracts/My.sol
+++ accessing-privated-data/contracts/My.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.19;
 
 contract My {
+    //These are stored in the slot0
     bool private b;
     uint8 private x;
     uint16 private y;
+
+    //it is stored in slot 1
     uint256 private z;
 
     constructor() {
